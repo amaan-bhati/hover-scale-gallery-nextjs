2b6df70750d57862a4f6ebe2e753ce04c365c743
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2023-10-30 23:22:36 +0530

    updated code upload

---
+++ src/app/page.js
@@ -0,0 +1,23 @@
+import styles from './page.module.scss'
+import { projects } from '../data';
+import Image from 'next/image';
+import Double from '../components/double';
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <h1>Lorem ipsum dolor
+      sit amet, consectetur
+      adipiscing elit, sed do
+      eiusmod tempor incididunt
+      ut labore et dolore magna aliqua.</h1>
+      <div className={styles.gallery}>
+        <Double projects={[projects[0], projects[1]]}/>
+        <Double projects={[projects[2], projects[3]]} reversed={true}/>
+        <Double projects={[projects[4], projects[5]]}/>
+        <Double projects={[projects[6], projects[7]]} reversed={true}/>
+      </div>
+    </main>
+  )
+}
+
